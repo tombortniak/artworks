8fe0ea46c719a471e36d3145042e30487dc3c871
tombortniak <tombortniak@gmail.com>
2023-08-06 23:08:21 +0200

    Initial commit

---
+++ Artworks/ArtworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtworksApp.swift
+//  Artworks
+//
+//  Created by Tomasz Bortniak on 06/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ArtworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
