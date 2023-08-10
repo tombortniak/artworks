61eed2824fc6f118fe0a86dbd5d61dcedd91f5d9
tombortniak <tombortniak@gmail.com>
2023-08-10 22:07:41 +0200

    Add artist model

---
+++ Artworks/Models/Artist.swift
@@ -0,0 +1,15 @@
+//
+//  Artist.swift
+//  Artworks
+//
+//  Created by Tomasz Bortniak on 10/08/2023.
+//
+
+import Foundation
+
+class Artist: Codable {
+    var id: Int
+    var name: String
+    var country: String
+    var period: String
+}
