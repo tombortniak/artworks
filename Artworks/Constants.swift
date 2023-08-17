d78480232a41683c8ecf5c58dbb7d52a2a3b718d
tombortniak <tombortniak@gmail.com>
2023-08-17 23:18:55 +0200

    Add constants file

---
+++ Artworks/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Artworks
+//
+//  Created by Tomasz Bortniak on 17/08/2023.
+//
+
+import Foundation
+
+struct Constants {
+    struct API {
+        static let artistsURL = "http://srv21.mikr.us:30123/api/v1/artists"
+        static let artworksURL = "http://srv21.mikr.us:30123/api/v1/artworks"
+    }
+}
