8725e7547bd309718d88e76194c9d8419f4c2e59
tombortniak <tombortniak@gmail.com>
2023-08-21 22:38:14 +0200

    Add fetching data from API

--- Artworks/ArtworksAPI.swift
+++ Artworks/ArtworksAPI.swift
@@ -34,4 +34,28 @@ class ArtworksAPI {
         constructedURL.queryItems = queryParameters.map { URLQueryItem(name: $0, value: $1)}.sorted { $0.name < $1.name }
         return constructedURL.url!
     }
+
+    func getData<T: Decodable>(with queryParameters: [String:String], from url: URL) async throws -> [T] {
+        var endpointURL = url
+        if !queryParameters.isEmpty {
+            endpointURL = try appending(queryParameters: queryParameters, to: url)
+        }
+        let (data, _) = try await URLSession.shared.data(from: endpointURL)
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        let decoded = try decoder.decode([T].self, from: data)
+        return decoded
+    }
+
+    func getArtworks(queryParameters: [String:String] = [:]) async throws -> [Artwork] {
+        let url = Constants.API.artworksURL!
+        let artworks: [Artwork] = try await getData(with: queryParameters, from: url)
+        return artworks
+    }
+
+    func getArtists(queryParameters: [String:String] = [:]) async throws -> [Artist] {
+        let url = Constants.API.artistsURL!
+        let artists: [Artist] = try await getData(with: queryParameters, from: url)
+        return artists
+    }
 }
