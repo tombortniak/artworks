5f28a90f3c0023d27b178eb702d81acfc1cca7cd
tombortniak <tombortniak@gmail.com>
2023-10-11 19:43:32 +0200

    Update artwork model

--- Artworks/ArtworksAPI.swift
+++ Artworks/ArtworksAPI.swift
@@ -8,16 +8,6 @@
 import Foundation
 
 class ArtworksAPI {
-    static let artists: [Artist] = [
-        Artist(id: 1, name: "Leonardo da Vinci", country: "Italy", period: "Renaissance"),
-        Artist(id: 2, name: "Michelangelo", country: "Italy", period: "Renaissance")
-    ]
-
-    static let artworks: [Artwork] = [
-        Artwork(id: 1, title: "Mona Lisa", image: "image1", artistId: 1, location: "Louvre", city: "Paris", country: "France", continent: "Europe"),
-        Artwork(id: 2, title: "The Last Judgement", image: "image2", artistId: 2, location: "Sistine Chapel", city: "Vatican City", country: "Vatican City", continent: "Europe")
-    ]
-
     private func isQueryValid(with queryParameters: [String:String]) -> Bool {
         if queryParameters.keys.contains("") || queryParameters.values.contains("") {
             return false
