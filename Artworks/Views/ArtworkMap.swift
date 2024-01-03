fc17f529ced207e77216e3b269a29336bf7e0564
tombortniak <tombortniak@gmail.com>
2024-01-03 19:13:15 +0100

    Add bottom sheet with grouped artworks

--- Artworks/Views/ArtworkMap.swift
+++ Artworks/Views/ArtworkMap.swift
@@ -8,23 +8,48 @@
 import SwiftUI
 import MapKit
 
+struct CoordsGroupingKey: Hashable {
+    let latitude: Double
+    let longitude: Double
+}
+
 struct ArtworkMap: View {
+    @State var isSheetPresented = false
     @State var position = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: Constants.Map.earthCenterLatitude, longitude: Constants.Map.earthCenterLongitude), span: MKCoordinateSpan(latitudeDelta: Constants.Map.latitudeDelta, longitudeDelta: Constants.Map.longitudeDelta)))
+    var groupedArtworks = Dictionary(grouping: artworks, by: { CoordsGroupingKey(latitude: $0.latitude, longitude: $0.longitude)})
     var body: some View {
         Map(position: $position) {
-            ForEach(artworks) { artwork in
-                Annotation(artwork.title, coordinate: CLLocationCoordinate2D(latitude: artwork.latitude, longitude: artwork.longitude)) {
+            ForEach(Array(groupedArtworks.values), id: \.self) { artworks in
+                Annotation(artworks.first?.location ?? "", coordinate: CLLocationCoordinate2D(latitude: artworks.first!.latitude, longitude: artworks.first!.longitude)) {
                     ZStack {
                         Circle()
                             .fill(.background)
                         Image(systemName: Constants.Map.annotationIcon)
                             .padding(5)
                     }
+                    .onTapGesture {
+                        isSheetPresented = true
+                    }
                 }
                 .annotationTitles(.hidden)
             }
         }
         .mapStyle(.hybrid(elevation: .realistic))
+        .sheet(isPresented: $isSheetPresented) {
+            VStack {
+                Text(artworks.first!.location)
+                    .font(.title2)
+                    .bold()
+                List(artworks, id: \.self) { artwork in
+                    ArtworkCard(artwork: artwork)
+                        .listRowSeparator(.hidden)
+                }
+                .listStyle(PlainListStyle())
+            }
+            .padding([.bottom, .top], 25)
+            .presentationDetents([.medium])
+            .presentationDragIndicator(.visible)
+        }
     }
 }
 
